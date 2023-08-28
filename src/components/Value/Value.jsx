203b52bb5291960243d2eedc6d47f3db2dc4aaef
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-08-28 23:47:46 +0100

    + company values section

--- src/components/Value/Value.jsx
+++ src/components/Value/Value.jsx
@@ -1,7 +1,58 @@
+import simple from './../../assets/simple.png'
 
 const Value = () => {
     return (
-        <div>Value</div>
+        <div className="mb-1 mt-2">
+            <h1 className="text-textColor text-2xl py-8 pb-[3rem] font-bold w-[400px] block">
+                The value that holds us true and to account
+            </h1>
+
+            <div className="grid gap-40 grid-cols-3 items-center">
+
+                <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+                    <div className="flex items-center gap-3">
+                        <div className="imgDiv p-1 rounded-[.8rem] bg-inherit-[#dedef8] h-10 w-10 flex items-center">
+                            <img src={simple} alt="" className="w-[100%]" />
+                        </div>
+                        <span className="font-semibold text-textColor text-[18px]">
+                            Simplicity
+                        </span>
+                    </div>
+                    <p className="text-[13px] text-textColor opacity-[.7] py-4 font-semibold ">
+                        Things being made beautiful simple are at the heart of everything we do.
+                    </p>
+                </div>
+
+                <div className="singleGrid rounded-[10px] hover:bg-[#f7edf5] p-[1.5rem]">
+                    <div className="flex items-center gap-3">
+                        <div className="imgDiv p-1 rounded-[.8rem] bg-inherit-[#dedef8] h-10 w-10 flex items-center">
+                            <img src={simple} alt="" className="w-[100%]" />
+                        </div>
+                        <span className="font-semibold text-textColor text-[18px]">
+                            Simplicity
+                        </span>
+                    </div>
+                    <p className="text-[13px] text-textColor opacity-[.7] py-4 font-semibold ">
+                        We believe in making things better or everonz, even if just by a little bit!
+                    </p>
+                </div>
+
+                <div className="singleGrid rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]">
+                    <div className="flex items-center gap-3">
+                        <div className="imgDiv p-1 rounded-[.8rem] bg-inherit-[#f3f2ad] h-10 w-10 flex items-center">
+                            <img src={simple} alt="" className="w-[100%]" />
+                        </div>
+                        <span className="font-semibold text-textColor text-[18px]">
+                            Simplicity
+                        </span>
+                    </div>
+                    <p className="text-[13px] text-textColor opacity-[.7] py-4 font-semibold ">
+                        We work on the basis of creating trust which can be nurtured through authenticity and transparency
+                    </p>
+                </div>
+
+            </div>
+        </div>
     )
 }
 
