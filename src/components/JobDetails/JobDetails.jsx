c90ef30d3c45fb1985e96e27b872f3a144a00f3a
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-08-30 00:19:46 +0100

    jobDetails: to be optimized

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,4 +1,5 @@
 import { BiBookmarkAlt } from "react-icons/bi";
+import { IoBagRemoveSharp } from "react-icons/io5";
 import { CiLocationOn } from "react-icons/ci";
 import { useLocation } from "react-router-dom"
 
@@ -8,9 +9,9 @@ const JobDetails = () => {
     const { id, image, title, location, desc, company, type, level, createdAt } = locate.state;
 
     return (
-        <div className="grid gap-10 bg-greyIsh rounded-[10px] p-[3rem] hover:shadow-lg mb-18">
+        <div className="grid gap-4 bg-slate-50 rounded-[10px] p-12 hover:shadow-lg mb-18">
             
-            <div className="header border-b-[2px] h-fit pb-1 flex justify-between ">
+            <div className="border-b-[2px] h-fit pb-1 flex justify-between ">
 
                 <div className="left">
                     <h1 className="text-3xl font-semibold text-textColor group-hover:text-white">{title}</h1>
@@ -31,6 +32,25 @@ const JobDetails = () => {
                     <CiLocationOn className=" text-2xl " />
                     <h6 className="text-xl text-[#ccc] ">{location}</h6>
                 </div>
+            </div>
+
+            <div className="p-1 border-b">
+                <h6 className="text-black text-xl font-semibold">Job details</h6>
+                <p className="text-textColor text-[15px]">Find out how the criteria for this position match your job preferences.</p>
+                <p className="text-textColor text-[15px]">Published on {createdAt}.</p>
+
+                <div className="my-2 flex gap-2 items-center">
+                    <IoBagRemoveSharp className="text-xl text-gray-800 "/>
+                    <h6 className="text-[18px] text-gray-800 font-semibold"> Job type</h6>
+                </div>
+
+                <h6 className="w-fit ml-6 py-1 px-4 font-semibold bg-gray-700 border text-white rounded">{type}</h6>
+            </div>
+
+            <div>
+                <h6 className="text-black text-xl py-2 font-semibold">Full job description</h6>
+                <h6 className="text-[17px] py-2 font-semibold text-textColor">{title}</h6>
+                <p className="text-textColor">{desc}</p>
             </div>
 
         </div>
--- src/data.js
+++ src/data.js
@@ -29,9 +29,9 @@ const data = [
     {
         id: 3,
         image: logo3,
-        title: "Full Stack Engineer",
+        title: "Project Manager",
         location: "Germany",
-        desc: "Seeking a talented Full Stack Engineer to work on end-to-end development projects. You'll have the opportunity to contribute to both frontend and backend components of our applications.",
+        desc: "At Flex Living, we believe renting a home should be as easy as buying an item from Amazon, this means ensuring our guests have an abundance of choice and flexibility to cancel if their plans change. We aim to help landlords stop paying excessive management fees and help renters live and work comfortably anywhere in the world. Since our launch in 2019, we have doubled in size every six months, and have already launched Flex Living in multiple cities worldwide. You’ll be joining us during a very exciting stage of development within our company as we launch Flex Living in Casablanca.About the role:                As site supervisor, you would be the most senior member of our ground operational team. Your daily tasks would be varied but your main responsibilities would include:                Property Refurbishment + Maintenance        Manage multiple contractors across multiple apartments in the delivery of painting, decorating and refurbishment projects        Quality Control on all projects        Providing training to new members of staff        Ordering supplies including equipment and furniture        Coordinating photoshoots of completed properties        Assisting with maintenance and guiding the maintenance team on the delivery of repair        About You:                Project Manager is a dynamic and challenging role, do the following characteristics describe you? If so then this role could be perfect for you.                We are looking for someone who:                Is a natural leader        Is a problem solver        Has excellent attention to detail        Is committed to completing tasks to a high standard        Is willing to go above and beyond        Wants to grow and develop with a rapidly expanding company",
         company: "CodeWorks",
         type: "Full-time",
         level: "Experienced",
@@ -91,6 +91,17 @@ const data = [
         type: "Part-time",
         level: "Beginner",
         createdAt: "2023-07-29"
+    },
+    {
+        id: 9,
+        image: logo3,
+        title: "Full Stack Engineer",
+        location: "Germany",
+        desc: "Seeking a talented Full Stack Engineer to work on end-to-end development projects. You'll have the opportunity to contribute to both frontend and backend components of our applications.",
+        company: "CodeWorks",
+        type: "Full-time",
+        level: "Experienced",
+        createdAt: "2023-08-26"
     }
 ];
 
