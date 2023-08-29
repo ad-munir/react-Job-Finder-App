52f12de7977fdad57dc0b19abee5ae37b405374f
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-08-29 21:33:35 +0100

    starting jobDetails

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -2,12 +2,18 @@ import { useLocation } from "react-router-dom"
 
 const JobDetails = () => {
 
-    const location = useLocation();
-  const from  = "i";
-  console.log(location.state);
+    const locate = useLocation();
+    const { id, image, title, location, desc, company, type, level, createdAt } = locate.state;
 
     return (
-        <div>from {from}</div>
+        <div className="grid gap-10 bg-greyIsh rounded-[10px] p-[3rem] hover:shadow-lg mb-18">
+            <div className="header border-b-[2px] ">
+                <h1 className="text-3xl font-semibold text-textColor group-hover:text-white">{title}</h1>
+
+                <h6 className="text-xl text-blueColor border-b w-fit border-blue-400">{company}</h6>
+                <h6 className="text-xl text-[#ccc]">{location}</h6>
+            </div>
+        </div>
     )
 }
 
